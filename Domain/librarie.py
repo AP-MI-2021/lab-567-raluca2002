80cf2efc9e0eea9920d5381e83da4bae9c79a9b1
Enea Raluca <raluca0602372yahoo.com>
2021-10-27 13:30:27 +0300

    am rezolvat

--- Domain/librarie.py
+++ Domain/librarie.py
@@ -1,62 +1,27 @@
-def creeaza_librarie(id_librarie: int,tiltu_carte,gen_carte,pret,tip_reducere_client ) :
-    """
-    Creeaza o vanzare pentru carte
-    :param id_carte: Id ul cartii
-    :param tiltu_carte: Titlul cartii
-    :param gen_carte: Genul cartii
-    :param pret: Pretul cartii
-    :param tip_reducere_client: Tip reducere client(none,silver,gold)
-    :return: O vanzare
-    """
-
-    return {
-        'id': id_librarie,
-        'titlu': tiltu_carte,
-        'gen': gen_carte,
-        'pret': pret,
-        'reducere':  tip_reducere_client,
-    }
+def creeaza_librarie(id_librarie: int,tiltu_carte: str,gen_carte: str,pret: int,tip_reducere_client: str):
+    lista_librarie= [id_librarie, tiltu_carte, gen_carte, pret, tip_reducere_client]
+    return lista_librarie
 
 
-def get_id(librarie):
-    """
-    Getter pentru id ul librariei
-    :param vanzare: cartea
-    :return: id ul librariei
-    """
-    return librarie['id']
+def get_id(lista_librarie):
 
-def get_titlu_carte(librarie):
-    '''
-    TO DO
-    :param librarie:
-    :return:
-    '''
-    return librarie['titlu']
+    return lista_librarie[0]
 
-def get_gen_carte(librarie):
-    '''
-    TO DO
-    :param librarie:
-    :return:
-    '''
-    return librarie['gen']
+def get_titlu_carte(lista_librarie):
 
-def get_pret(librarie):
-    '''
-    TO DO
-    :param librarie:
-    :return:
-    '''
-    return librarie['pret']
+    return lista_librarie[1]
 
-def get_tip_reducere_client(librarie):
-    '''
-    TO DO
-    :param librarie:
-    :return:
-    '''
-    return librarie['reducere']
+def get_gen_carte(lista_librarie):
+
+    return lista_librarie[2]
+
+def get_pret(lista_librarie):
+
+     return lista_librarie[3]
+
+def get_tip_reducere_client(lista_librarie):
+
+    return lista_librarie[4]
 
 def get_str(librarie):
     return f'Libraria cu id-ul {get_id(librarie)}, cu titlul {get_titlu_carte(librarie)}, genul {get_gen_carte(librarie)}, pretul {get_pret(librarie)} si tipul de reducere {get_tip_reducere_client(librarie)}'
